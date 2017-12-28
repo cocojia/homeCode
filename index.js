633737f952ef393deeb1c8f560087ba537efc58b
lijuanl <lijuanl@wicresoft.com>
2017-12-28 16:20:51 +0800

    add index.html

---
+++ index.js
@@ -0,0 +1,23 @@
+<script>
+
+/*============================用rem做响应式布局时，js控制fontsize============================*/
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            if (clientWidth >= 640) {     //640为设计图尺寸 
+                docEl.style.fontSize = '100px';
+            } else {
+                docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+            }
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
+/*============================用rem做响应式布局时，js控制fontsize============================*/
+
+</script>
